f039fbecc7ae9e5a0da089f77688c36d91922416
Layton Wang <wangzixu.china@gmail.com>
2017-11-04 00:16:49 +0800

    Optimize power of 2 using template metaprog

--- src/utils.hpp
+++ src/utils.hpp
@@ -60,6 +60,18 @@ constexpr std::size_t isqrt(void) {
   return x >> (numOfZeros / 2);
 }
 
+template <std::size_t x>
+constexpr std::size_t pow2(void) {
+  static_assert(isPowerOf2(x), "Size is not a power of 2!");
+  auto tmp = x;
+  std::size_t numOfZeros = 0;
+  while (tmp != 1) {
+    tmp >>= 1;
+    numOfZeros ++;
+  }
+  return x << numOfZeros;
+}
+
 template <typename ActionType>
 inline ActionType positionToAction(const std::size_t x, const std::size_t y) {
   _sanityCheck<ActionType>();
@@ -70,8 +82,7 @@ template <typename ActionType>
 inline auto actionToBoard(const ActionType action) {
   _sanityCheck<ActionType>();
   using BitBoardType = typename
-    BitType<std::numeric_limits<ActionType>::digits *
-            std::numeric_limits<ActionType>::digits>::type;
+    BitType<pow2<std::numeric_limits<ActionType>::digits>()>::type;
   return static_cast<BitBoardType>(1)
           << (std::numeric_limits<BitBoardType>::digits - 1 - action);
 }
